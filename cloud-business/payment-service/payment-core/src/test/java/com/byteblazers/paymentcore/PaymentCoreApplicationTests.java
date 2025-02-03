0da2cc3faef9ff96496d3a02f00227e6f8e4a967
zhangjk <2878033347@qq.com>
2025-02-03 20:04:59 +0800

    feat. 更新依赖

---
+++ cloud-business/payment-service/payment-core/src/test/java/com/byteblazers/paymentcore/PaymentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteblazers.paymentcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCoreApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
